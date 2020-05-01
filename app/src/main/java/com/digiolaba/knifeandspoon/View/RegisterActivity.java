5792ba74c0d4cc698f35e1b08335a206930505fa
MaxGio98 <48352868+MaxGio98@users.noreply.github.com>
2020-05-01 13:24:51 +0200

    Aggiunto metodo checkIfNameExists: verifica se esiste o meno un determinato nome utente nel db.

--- app/src/main/java/com/digiolaba/knifeandspoon/View/RegisterActivity.java
+++ app/src/main/java/com/digiolaba/knifeandspoon/View/RegisterActivity.java
@@ -14,14 +14,22 @@ import android.widget.Toast;
 
 import com.digiolaba.knifeandspoon.Controller.Utils;
 import com.digiolaba.knifeandspoon.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -67,42 +75,66 @@ public class RegisterActivity extends AppCompatActivity {
         }
         else
         {
-            registerUser();
+            checkIfNameExists();
         }
+    }
+
+
+    private void checkIfNameExists()
+    {
+        final Context context=this;
+        FirebaseFirestore rootRef=FirebaseFirestore.getInstance();
+        CollectionReference utentiRef=rootRef.collection("Utenti");
+        Query queryNome=utentiRef.whereEqualTo("Nome",nome.getText().toString());
+        queryNome.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful())
+                {
+                    for(DocumentSnapshot documentSnapshot:task.getResult())
+                    {
+                        if(documentSnapshot.exists())
+                        {
+                            Utils.errorDialog(context, R.string.error_name_already_taken,R.string.error_ok);
+                        }
+                    }
+                }
+                if(task.getResult().size()==0)
+                {
+                    registerUser();
+                }
+            }
+        });
     }
 
     private void registerUser(){
         FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
         FirebaseUser fireUser = firebaseAuth.getCurrentUser();
-        if(nome.getText().length()==0){
-            showToastMessage("Il nome non può essere vuoto");
-        }else{
-            Log.d(TAG, "SHIIIT");
-            Map<String, Object> user = new HashMap<>();
-            user.put("Mail", fireUser.getEmail().toString());
-            user.put("Nome", nome.getText().toString());
-            user.put("Immagine", "");
-            user.put("isAdmin", false);
-            Log.d(TAG, "SHIIIT");
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            Log.d(TAG, "SHIIIT");
-            // Add a new document with a generated ID
-            db.collection("Utenti")
-                    .add(user)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                            launchMainActivity();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(TAG, "Error adding document", e);
-                        }
-                    });
-        }
+        Log.d(TAG, "SHIIIT");
+        Map<String, Object> user = new HashMap<>();
+        user.put("Mail", fireUser.getEmail().toString());
+        user.put("Nome", nome.getText().toString());
+        user.put("Immagine", "");
+        user.put("isAdmin", false);
+        Log.d(TAG, "SHIIIT");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Log.d(TAG, "SHIIIT");
+        // Add a new document with a generated ID
+        db.collection("Utenti")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        launchMainActivity();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
     private void launchMainActivity(){
